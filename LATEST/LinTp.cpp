a3bb500612b5dbc8939b54be79bd1f1147cff01e
root <nagaraja.hm.0001@gmail.com>
2022-01-19 12:35:25 +0100

    Update: submodule LinTp

--- LATEST/LinTp.cpp
+++ LATEST/LinTp.cpp
@@ -2,24 +2,24 @@
 
 class_LinTp LinTp;
 
-FUNC(void, LINTP_CODE) class_Adc::InitFunction(void){
+FUNC(void, LINTP_CODE) class_LinTp::InitFunction(void){
 }
 
-FUNC(void, LINTP_CODE) class_Adc::GetVersionInfo(void){
+FUNC(void, LINTP_CODE) class_LinTp::GetVersionInfo(void){
 }
 
-FUNC(void, LINTP_CODE) class_Adc::Transmit(void){
+FUNC(void, LINTP_CODE) class_LinTp::Transmit(void){
 }
 
-FUNC(void, LINTP_CODE) class_Adc::Shutdown(void){
+FUNC(void, LINTP_CODE) class_LinTp::Shutdown(void){
 }
 
-FUNC(void, LINTP_CODE) class_Adc::CancelTransmit(void){
+FUNC(void, LINTP_CODE) class_LinTp::CancelTransmit(void){
 }
 
-FUNC(void, LINTP_CODE) class_Adc::ChangeParameter(void){
+FUNC(void, LINTP_CODE) class_LinTp::ChangeParameter(void){
 }
 
-FUNC(void, LINTP_CODE) class_Adc::CancelReceive(void){
+FUNC(void, LINTP_CODE) class_LinTp::CancelReceive(void){
 }
 

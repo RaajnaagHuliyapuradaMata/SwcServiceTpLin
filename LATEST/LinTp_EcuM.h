cdb91221b194dcd00df0bc3aca36ca2861d76e24
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:06:42 +0100

    Update: submodule LinTp

--- LATEST/LinTp_EcuM.h
+++ LATEST/LinTp_EcuM.h
@@ -27,8 +27,8 @@ class interface_LinTp_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, LINTP_CODE) InitFunction   (void);
-      FUNC(void, LINTP_CODE) DeInitFunction (void);
+      virtual FUNC(void, LINTP_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, LINTP_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/

5a0eb508e54f2b9263499f6c344dee312cd4368d
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:13 +0100

    Update: AUTOSAR basic software ECU abstraction module LinTp

--- LATEST/LinTp.cpp
+++ LATEST/LinTp.cpp
@@ -40,6 +40,16 @@ class module_LinTp:
       FUNC(void, LINTP_CODE) DeInitFunction (void);
       FUNC(void, LINTP_CODE) GetVersionInfo (void);
       FUNC(void, LINTP_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, LINTP_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/

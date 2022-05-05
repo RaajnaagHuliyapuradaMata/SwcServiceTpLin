22400100d54d282e85fa9dd9f61d60a1b34b92f2
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:07:35 +0000

    Update: AUTOSAR basic software ECU abstraction module LinTp

--- LATEST/LinTp.cpp
+++ LATEST/LinTp.cpp
@@ -85,12 +85,10 @@ FUNC(void, LINTP_CODE) module_LinTp::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgLinTp;
          }
       }

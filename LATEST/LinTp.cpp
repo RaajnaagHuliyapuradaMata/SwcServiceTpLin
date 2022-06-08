d848103238b435e6fcc6032d90070affa45f645e
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-08 21:18:36 +0000

    Update: AUTOSAR basic software ECU abstraction module LinTp

--- LATEST/LinTp.cpp
+++ LATEST/LinTp.cpp
@@ -75,7 +75,7 @@ CONSTP2VAR(infSchMClient, LINTP_VAR, LINTP_CONST) gptrinfSchMClient_LinTp = &Lin
 VAR(module_LinTp, LINTP_VAR) LinTp(
    {
 #if(STD_ON == _ReSIM)
-// char strModuleName[6];
+   char strModuleName[6] = "LINTP";
 #else
 #endif
          LINTP_AR_RELEASE_VERSION_MAJOR

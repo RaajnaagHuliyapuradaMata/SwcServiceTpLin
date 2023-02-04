fc0d45828c89f0cafc5aa37156a00cc08d15fdc8
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:30:46 +0100

    Update: AUTOSAR basic software ECU abstraction module for LIN communication protocol stack - transport layer

--- InfineonTLE9879EvalKit_R2023/infEcuabLinTp_ServiceNvM_Types.hpp
+++ InfineonTLE9879EvalKit_R2023/infEcuabLinTp_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_EcuabLinTp_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_EcuabLinTp_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
